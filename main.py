2827a23fb75aca6f9051577f6a9311b60ca23ab8
shaib <shai.baranes@gmail.com>
2025-06-20 18:20:49 +0300

    Changing H1 Header

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-st.write("# This is a H1 header!")
+st.write("# This is a H1 header!...")
 st.write("## This is a H2 header!")
 st.write("### This is a H3 header!")
 
